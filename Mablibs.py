ee0b4e8acfeebdf89cd217f5c40e7304318c49e2
SwaThiPadmanabh17 <swathipadmanabh312@gmail.com>
2021-05-10 23:52:01 +0530

    basic progams

---
+++ Mablibs.py
@@ -0,0 +1,12 @@
+day = input("Please enter a day: ")
+number = input("Plese enter a number: ")
+name = input("Plese enter a name: ")
+animal = input("Please enter an animal: ")
+relative = input("Please enter a relative: ")
+
+
+madlibs = f"On the {day}, I was going to {name}'s house and on the way" \
+          f" I saw {number} {animal}(s) and I was shocked and I ran and " \
+          f"informed my {relative} who helped me in chasing the {animal} "
+
+print(madlibs)
