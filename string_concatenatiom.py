26b2af37a039ae0a5d3ee662e541d809a920d5b8
SwaThiPadmanabh17 <swathipadmanabh312@gmail.com>
2021-05-10 23:18:28 +0530

    basic progams

---
+++ string_concatenatiom.py
@@ -0,0 +1,6 @@
+channel_name = "kaliyona"
+
+print("Please subscribe to our channel: " + channel_name)
+print("please subscribe to our channel:  {}".format(channel_name))
+print(f"Please subscribe to our channel: {channel_name}")
+
